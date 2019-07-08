850c83a24aa7c60e69e0dfbfbbb261e5cbcabd98
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-08 12:15:55 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,32 @@
+class Dog
+  attr_accessor :id, :name, :breed
+  
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+  
+  def save
+    sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
+    DB[:conn].execute(sql, self.name, self.breed)
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    self
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE dogs")
+  end
+  
+end
